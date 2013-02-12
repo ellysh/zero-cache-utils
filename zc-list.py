948e5b89db6056cd6a5065f8a5411113f6b320a7
Ilya Shpigor <petrsum@gmail.com>
2013-02-12 09:42:46 +0400

    Implement formatted list of keys and values in output

--- zc-list.py
+++ zc-list.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 
+import sys
 import client_wrap
 
-def main():
-    client = client_wrap.ClientWrap("get_test.log", "ipc:///var/run/zero-cache/0", 0)
-
+def get_keys(client):
     key_str = client.GetKeys()
     keys = key_str.split (';')
     del keys[-1]
 
     if len(keys) == 0:
-        return
+        sys.exit()
 
-    print keys
+    return keys
+
+def print_keys(client, keys):
+    for key in keys:
+        value = client.ReadLong(key)
+        print "%s = %d" % (key, value)
+
+def main():
+    client = client_wrap.ClientWrap("get_test.log", "ipc:///var/run/zero-cache/0", 0)
+
+    keys = get_keys(client)
+
+    print_keys(client, keys)
 
 if __name__ == "__main__":
     main()
