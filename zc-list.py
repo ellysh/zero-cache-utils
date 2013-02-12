3beea9e3800f5c1e68f869d46d137162016a5276
Ilya Shpigor <petrsum@gmail.com>
2013-02-12 10:27:01 +0400

    Add the parsing arguments function

--- zc-list.py
+++ zc-list.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 import client_wrap
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--type", help="type of the cached data", default="double")
+    parser.add_argument("-c", "--connection", help="connection string", default="ipc:///var/run/zero-cache/0")
+    parser.add_argument("-w", "--column", help="number of columns", type=int, default=1)
+    parser.add_argument("-l", "--log", help="log file name", default="")
+    args = parser.parse_args()
 
 def get_keys(client):
     key_str = client.GetKeys()
@@ -19,10 +28,9 @@ def print_keys(client, keys):
         print "%s = %d" % (key, value)
 
 def main():
+    parse_args()
     client = client_wrap.ClientWrap("get_test.log", "ipc:///var/run/zero-cache/0", 0)
-
     keys = get_keys(client)
-
     print_keys(client, keys)
 
 if __name__ == "__main__":
