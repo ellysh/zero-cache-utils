97168d88fbfe7993fa86842d2dd392478423ce86
Ilya Shpigor <petrsum@gmail.com>
2013-02-12 13:12:26 +0400

    Add command line arguments for the output's alignment

--- zc-list.py
+++ zc-list.py
@@ -10,20 +10,24 @@ READ_FUNCTIONS = {
     'string' : client_wrap.ClientWrap.ReadString
 }
 
-PRINT_FORMATS = {
-    'double' : "%s = %f",
-    'long' : "%s = %d",
-    'string' : "%s = %s",
-}
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--type", help="type of the cached data", default="double")
     parser.add_argument("-c", "--connection", help="connection string", default="ipc:///var/run/zero-cache/0")
     parser.add_argument("-w", "--column", help="number of columns", type=int, default=1)
+    parser.add_argument("-k", "--align-key", help="symbol count for key", default="5")
+    parser.add_argument("-v", "--align-value", help="symbol count for value", default="20")
     parser.add_argument("-l", "--log", help="log file name", default="")
+
     global ARGS
     ARGS = parser.parse_args()
+
+    global PRINT_FORMATS
+    PRINT_FORMATS = {
+        'double' : "%-" + ARGS.align_key + "s = %-" + ARGS.align_value + "f",
+        'long' : "%-" + ARGS.align_key + "s = %-" + ARGS.align_value + "d",
+        'string' : "%-" + ARGS.align_key + "s = %-" + ARGS.align_value + "s",
+    }
 
 def get_keys(client):
     key_str = client.GetKeys()
