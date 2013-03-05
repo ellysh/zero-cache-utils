085dc8ca865dfcb10b7d4bf57146e1aba5f6021c
Ilya Shpigor <petrsum@gmail.com>
2013-03-05 11:18:40 +0400

    Fix `zc-set` utility according new zero-cache 2.0 API

--- zc-set.py
+++ zc-set.py
@@ -2,32 +2,36 @@
 
 import sys
 import argparse
-import client_wrap
+import zero_cache
 
 WRITE_FUNCTIONS = {
-    'double' : client_wrap.ClientWrap.WriteDouble,
-    'long' : client_wrap.ClientWrap.WriteLong,
-    'string' : client_wrap.ClientWrap.WriteString
+    'double' : zero_cache.Client.WriteDouble,
+    'long' : zero_cache.Client.WriteLong
+}
+
+CONVERSION_FUNCTIONS = {
+    'double' : float,
+    'long' : int
 }
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="This is utility to set new value for the specified key in the cache")
-    parser.add_argument("-t", "--type", help="type of the cached data", default="double", choices=('double', 'long', 'string'))
-    parser.add_argument("-c", "--connection", help="connection string", default="ipc:///var/run/zero-cache/0")
-    parser.add_argument("-k", "--key", help="key string", required=True)
+    parser = argparse.ArgumentParser(description="This is utility to set new value for the specified index in the cache")
+    parser.add_argument("-t", "--type", help="type of the cached data", default="double", choices=('double', 'long'))
+    parser.add_argument("-i", "--index", help="index number", type=int, required=True)
     parser.add_argument("-v", "--value", help="cached value", required=True)
     parser.add_argument("-l", "--log", help="log file name", default="")
 
     global ARGS
     ARGS = parser.parse_args()
 
-def write_value(client, key, value):
-    return WRITE_FUNCTIONS[ARGS.type](client, key, value)
+def write_value(client, index, value):
+    return WRITE_FUNCTIONS[ARGS.type](client, index, value)
 
 def main():
     parse_args()
-    client = client_wrap.ClientWrap(ARGS.log, ARGS.connection, 0)
-    write_value(client, ARGS.key, ARGS.value)
+    client = zero_cache.Client(ARGS.log)
+    value = CONVERSION_FUNCTIONS[ARGS.type](ARGS.value)
+    write_value(client, ARGS.index, value)
 
 if __name__ == "__main__":
     main()
